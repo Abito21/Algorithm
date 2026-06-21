c9da439e6d5a4638ea345c52ef1ad5db98d709f1
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2026-06-21 21:21:30 +0700

    Create splitStrings.py

---
+++ codewars/splitStrings.py
@@ -0,0 +1,18 @@
+"""
+Problem:
+Complete the solution so that it splits the string into strings of two characters in a list/array (depending on the language you use). If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+"""
+
+# Solution:
+def solution(s):
+  if len(s) % 2 == 1:
+    s += '_'
+  arrChar = []
+  for char in range(0, len(s), 2):
+    arrChar.append(s[char:char+2])
+  return arrChar
